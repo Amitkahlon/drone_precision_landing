ecd782589d119e6272602ee45eaf399b26168d0e
Amit Kahlon <amitkahlon4321@gmail.com>
2026-05-29 21:18:48 +0200

    added mission1 and mission2

--- controls/mission.py
+++ controls/mission.py
@@ -1,8 +1,20 @@
 import numpy as np
-import mujoco
+
+from .platform import Platform, Waypoint
+from .moving_platform import MovingPlatform
 
 
 class Mission:
-    def __init__(self, start, goal):
-        self.start = np.array(start, dtype=float)
-        self.goal  = np.array(goal,  dtype=float)
+    def __init__(self, start: tuple, platform: Platform):
+        self.start     = np.array(start, dtype=float)
+        self._platform = platform
+        self._checkpoints: list[Waypoint] = []
+
+    def add_checkpoint(self, position: tuple, speed: float) -> 'Mission':
+        self._checkpoints.append(Waypoint(position, speed))
+        return self
+
+    def build_platform(self) -> MovingPlatform:
+        if len(self._checkpoints) < 2:
+            raise ValueError("Mission needs at least 2 checkpoints to build a moving platform")
+        return MovingPlatform(self._platform, self._checkpoints)
